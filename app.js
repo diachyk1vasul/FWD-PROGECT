d9f4315d07b98cc762ae6576e57619e4e06fbeb0
Василь Дячук <58522978+diachyk1vasul@users.noreply.github.com>
2020-02-07 16:38:07 +0200

    add

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
+const path = require("path");
+const app = express();
+
+//View engine setup
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+//Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Static folder
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("contact", { layout: false });
+});
+
+app.listen(3000, () => console.log("Server started..."));
